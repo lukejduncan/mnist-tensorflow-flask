f13a7f5a6abfe2682eb76b934755567ff6224637
Luke Duncan <lukejduncan@gmail.com>
2017-01-24 20:43:59 -0800

    Add API for uploading pngs directly
    The porblem statement was for uploading pngs directly to the API and not
    vectors.  The API has been updated to support this, as have the tools
    and the README instructions

--- mnist/app/app.py
+++ mnist/app/app.py
@@ -1,6 +1,8 @@
 from flask import abort, Flask, jsonify, request
-from ..model import softmax as mnist
+import io
 import numpy as np
+from ..model import softmax as mnist
+import png
 import tensorflow as tf
 
 app = Flask(__name__)
@@ -8,8 +10,39 @@ app = Flask(__name__)
 sess = tf.Session()
 graph = mnist.load(sess)
 
+ALLOWED_EXTENSION = ['png']
+
+def allowed_file(filename):
+  return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSION
+
 @app.route('/mnist/classify', methods=['POST'])
 def classify():
+  if 'file' not in request.files:
+    abort(401)
+
+  file = request.files['file']
+
+  if not allowed_file(file.filename):
+    abort(402)
+
+  #in_memory_file = io.BytesIO()
+  #file.save(in_memory_file)
+  r = png.Reader(file=file.stream)
+  img = np.array(list(r.read()[2]))
+  
+  (row, col) = img.shape
+  if row*col != 784:
+    abort(403)
+
+  img = img.reshape(1,784)
+
+  prediction = mnist.predict(sess, graph, img)
+  response = {'classification': prediction[0]}  
+  return jsonify(**response)
+
+
+@app.route('/mnist/classify-vector', methods=['POST'])
+def classify_vector():
   try:
     img = request.form['img'].split(',')
   except KeyError:
--- query.py
+++ query.py
@@ -1,15 +1,25 @@
 from __future__ import print_function
+
+import idx2numpy, numpy, png
 import mnist.model.softmax as mnist
 import numpy as np
+import os
 import requests
 
 imgs, _ = mnist.mnist_test_img.shape
 idx = np.random.choice(range(imgs))
 
-img = ','.join(['%d' % num for num in mnist.mnist_test_img[idx]])
-query = {'img': img}
+path = '/tmp/mnist-image.png'
 
-r = requests.post('http://localhost:5000/mnist/classify', data=query)
+png.from_array(mnist.mnist_test_img[idx].reshape(28, 28), 'L').save(path)
+
+f = open(path, 'rb')
+files = {'file': open(path, 'rb')}
+
+r = requests.post('http://localhost:5000/mnist/classify', files=files)
 print("True Class: %d" % np.argmax(mnist.mnist_test_label[idx]))
 print("HTTP Response:")
 print(r.text)
+
+f.close()
+os.remove(path)
